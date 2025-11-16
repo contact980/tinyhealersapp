e4c98ee63a179aab3f337882af9f6f99f46610a1
Naresh984 <tnaresh564@gmail.com>
2025-11-16 20:17:00 +0530

    latet bug fixes v4

--- src/pages/DoctorDetails.js
+++ src/pages/DoctorDetails.js
@@ -123,7 +123,7 @@ const DoctorDetails = ({ doctor = null, mode = 'view', onClose = () => {} }) => 
     };
   }, [onClose]);
 
-  // determine if current local user is role 3 (doctor) and should be view-onlyyyy
+  // determine if current local user is role 3 (doctor) and should be view-onlyyyyyyyyyy
   useEffect(() => {
     if (typeof window === 'undefined') return;
     try {
